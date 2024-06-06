0f67489a847c4ac4078c52c3e6a11aaad217d0e2
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-06-06 15:08:30 +0300

    Added experimental ts feature

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+  output: "standalone",
+  experimental: {
+    typedRoutes: true,
+  },
+};
 
 export default nextConfig;
