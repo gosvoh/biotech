21599b7bfd61859defe08bff090550391b052816
gosvoh <vokhmina2011@yandex.ru>
2024-11-02 12:40:18 +0300

    Add utility function to remove hanging prepositions and conjunctions from text

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import type React from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Функция для получения текста, в котором убраны висячие предлоги и союзы
+ * @argument text - текст, в котором нужно убрать висячие предлоги и союзы
+ * @returns текст без висячих предлогов и союзов с неразрывными пробелами
+ */
+export function removeHangingPrepositionsAndConjunctions(
+  text: TemplateStringsArray | string | React.ReactElement
+): string {
+  if (typeof text === "string") {
+    return text.replace(/(\s[а-яё]{1,3})\s/gi, "$1\u00A0");
+  }
+  if (Array.isArray(text)) {
+    return text.join("").replace(/(\s[а-яё]{1,3})\s/gi, "$1\u00A0");
+  }
+  if (typeof text === "object" && "props" in text) {
+    const textString = text.props.children as string;
+    return textString.replace(/(\s[а-яё]{1,3})\s/gi, "$1\u00A0");
+  }
+  return "ERROR REMOVE HANGING PREPOSITIONS AND CONJUNCTIONS";
 }
