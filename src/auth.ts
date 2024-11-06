1edbda05a896a04dc1710db2455c3aa51cf78b68
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-11-06 18:07:36 +0300

    Fix NextAuth configuration with Resend provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,14 +1,15 @@
 import NextAuth from "next-auth";
-import Resend from "next-auth/providers/resend";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/prisma";
+import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Resend],
+  session: { strategy: "jwt" },
   callbacks: {
     authorized: async ({ auth }) => {
       return !!auth;
     },
   },
+  ...authConfig,
 });
