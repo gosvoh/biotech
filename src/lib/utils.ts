4e4d5ff266ee8e6545257767238015565c376098
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-11-24 20:33:56 +0300

    Enhance dbAction function to support multiple tags and optional return value

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -28,6 +28,17 @@ export function removeHangingPrepositionsAndConjunctions(
   return "ERROR REMOVE HANGING PREPOSITIONS AND CONJUNCTIONS";
 }
 
-export function dbAction<T>(action: Promise<T>, tagToRevalidate: string) {
-  return action.then(() => revalidateTag(tagToRevalidate)).then(() => true);
+export function dbAction<T>(
+  action: Promise<T>,
+  tagToRevalidate: string | string[],
+  returnValue?: boolean
+) {
+  return action
+    .then((data) => {
+      if (Array.isArray(tagToRevalidate))
+        tagToRevalidate.forEach((tag) => revalidateTag(tag));
+      else revalidateTag(tagToRevalidate);
+      return data;
+    })
+    .then((data) => (returnValue ? data : true));
 }
