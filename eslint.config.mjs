93610d20d00d6b283ee347e9fda2687dca3e3ef8
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-12-20 23:28:40 +0300

    Update ESLint configuration and dependencies; remove ignored paths and upgrade ESLint version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["src/lib/db/client"],
-}, ...compat.extends("next/core-web-vitals", "next/typescript")];
+const eslintConfig = [
+  { ignores: ["src/lib/db/client"] },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
