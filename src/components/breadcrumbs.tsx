3354955a675ef0ec234539aa9e4143239ec621f9
Aleksey Vokhmin <vohmina2011@yandex.ru>
2025-03-26 00:41:04 +0300

    Enhance Breadcrumbs component with ChevronRight icon and improve layout structure

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
+import { ChevronRight } from "lucide-react";
 
 export default function Breadcrumbs({
   items,
@@ -13,35 +14,43 @@ export default function Breadcrumbs({
   return (
     <div
       className={cn(
-        "text-sm lg:text-base xl:text-xl wrapper w-full",
-        "hidden md:flex flex-row gap-2 xl:gap-4 items-center",
+        "text-sm lg:text-base wrapper w-full",
+        "hidden md:flex flex-row gap-2 items-center",
         "mb-6"
       )}
     >
       {items.map((item, i) => (
-        <div className="group contents" key={`breadcrumb-${i}`}>
+        <>
           <div
-            className={cn(
-              "bg-foreground rounded-full w-2 h-2",
-              item.href
-                ? "opacity-50 group-hover:opacity-100 group-hover:bg-day-primary-violet-hover group-active:bg-day-primary-violet-active transition-all duration-300 bg-opacity-[.04]"
-                : ""
-            )}
-          />
-          {item.href ? (
-            <Link
+            className="group flex flex-row items-center gap-1"
+            key={`breadcrumb-${i}`}
+          >
+            <div
               className={cn(
-                "opacity-50 group-hover:opacity-100 group-hover:text-day-primary-violet-hover group-active:text-day-primary-violet-active transition-all duration-300 min-w-fit",
-                "peer"
+                "bg-foreground rounded-full w-2 h-2",
+                item.href
+                  ? "opacity-50 group-hover:opacity-100 group-hover:bg-day-primary-violet-hover group-active:bg-day-primary-violet-active transition-all duration-300 bg-opacity-[.04]"
+                  : ""
               )}
-              href={item.href}
-            >
-              {item.title}
-            </Link>
-          ) : (
-            <p>{item.title}</p>
+            />
+            {item.href ? (
+              <Link
+                className={cn(
+                  "opacity-50 group-hover:opacity-100 group-hover:text-day-primary-violet-hover group-active:text-day-primary-violet-active transition-all duration-300 min-w-fit",
+                  "peer"
+                )}
+                href={item.href}
+              >
+                {item.title}
+              </Link>
+            ) : (
+              <p className="text-sm lg:text-base">{item.title}</p>
+            )}
+          </div>
+          {i !== items.length - 1 && (
+            <ChevronRight className="opacity-50 w-4 h-4" />
           )}
-        </div>
+        </>
       ))}
     </div>
   );
