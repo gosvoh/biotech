70cc363ffbb098e20c180b1d366fe8b160f32fc9
Aleksey Vokhmin <vohmina2011@yandex.ru>
2025-12-23 11:07:44 +0300

    Fix instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { execSync } = await import("child_process");
-    execSync("npm run db:deploy");
+    execSync("npx --yes prisma@6 migrate deploy");
   }
 }
