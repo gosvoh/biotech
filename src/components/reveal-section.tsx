d27513f99d8efdcf753ebd4207a2cd7d42eb0713
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-02-18 22:54:07 +0300

    Fix viewport trigger amount

--- src/components/reveal-section.tsx
+++ src/components/reveal-section.tsx
@@ -20,7 +20,7 @@ export default function RevealSection({
       {...props}
       initial={{ opacity: 0, y: 18 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.35, margin: "0px 0px -10% 0px" }}
+      viewport={{ once: true, amount: 0.1, margin: "0px 0px -10% 0px" }}
       transition={{ duration: 0.5, ease: [0.22, 1, 0.36, 1] }}
     >
       {children}
