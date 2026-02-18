7383416a1dc0eefa71b8d77dd252a9b900dfaa1d
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-02-18 12:35:43 +0300

    ESLint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,10 @@
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
+  ...nextCoreWebVitals,
+  ...nextTypescript,
   { ignores: ["src/lib/db/client"] },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
