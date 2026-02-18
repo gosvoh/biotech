866ce744ee0a2840015e68c7b3092b7f693e28f6
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-02-18 23:14:01 +0300

    Fixed og fonts

--- src/lib/meta.tsx
+++ src/lib/meta.tsx
@@ -6,82 +6,72 @@ import { readFile } from "fs/promises";
 
 export const generateOGImage = async (
   title: string,
-  textStyle?: React.CSSProperties
+  textStyle?: React.CSSProperties,
 ) => {
   const gorizontFont = readFile(
-    join(process.cwd(), "public/ALS Gorizont Variable.ttf")
-  )
-    .catch(() =>
-      readFile(join(process.cwd(), "src/app/fonts/ALS Gorizont Variable.ttf"))
-    )
-    .then((res) => Uint8Array.from(res).buffer);
+    join(process.cwd(), "public/ALSGorizont-BoldExpanded.otf"),
+  ).catch(() =>
+    readFile(join(process.cwd(), "src/app/fonts/ALSGorizont-BoldExpanded.otf")),
+  );
   const logoDataUrl = readFile(
     join(process.cwd(), "public/Biotech-Logotype.svg"),
-    "utf8"
-  ).then((svg) => `data:image/svg+xml;base64,${Buffer.from(svg).toString("base64")}`);
-  const [fontData, logoSrc] = await Promise.all([
-    gorizontFont,
-    logoDataUrl,
-  ]);
+    "utf8",
+  ).then(
+    (svg) => `data:image/svg+xml;base64,${Buffer.from(svg).toString("base64")}`,
+  );
+  const [fontData, logoSrc] = await Promise.all([gorizontFont, logoDataUrl]);
 
   return new ImageResponse(
-    (
+    <div
+      style={{
+        fontSize: 64,
+        backgroundColor: "hsl(0, 0%, 100%)",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "hsl(0, 0%, 0%)",
+        marginInline: "auto",
+      }}
+    >
       <div
         style={{
-          fontSize: 64,
-          backgroundColor: "hsl(0, 0%, 100%)",
+          maxWidth: 1250,
+          maxHeight: 660,
           width: "100%",
           height: "100%",
           display: "flex",
           flexDirection: "column",
+          position: "relative",
           justifyContent: "center",
           alignItems: "center",
-          color: "hsl(0, 0%, 0%)",
-          marginInline: "auto",
         }}
       >
-        <div
+        <picture
           style={{
-            maxWidth: 1250,
-            maxHeight: 660,
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            flexDirection: "column",
-            position: "relative",
-            justifyContent: "center",
-            alignItems: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: 300,
           }}
         >
-          <picture
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: 300,
-            }}
-          >
-            <img
-              src={logoSrc}
-              alt="Biotech"
-              width={300}
-              height={98}
-            />
-          </picture>
-          <p
-            style={{
-              textAlign: "center",
-              marginInline: "auto",
-              color: "hsl(270, 100%, 50%)",
-              alignSelf: "center",
-              ...textStyle,
-            }}
-          >
-            {title}
-          </p>
-        </div>
+          <img src={logoSrc} alt="Biotech" width={300} height={98} />
+        </picture>
+        <p
+          style={{
+            textAlign: "center",
+            marginInline: "auto",
+            color: "hsl(270, 100%, 50%)",
+            alignSelf: "center",
+            ...textStyle,
+          }}
+        >
+          {title}
+        </p>
       </div>
-    ),
+    </div>,
     {
       width: 1920,
       height: 960,
@@ -93,14 +83,14 @@ export const generateOGImage = async (
           weight: 400,
         },
       ],
-    }
+    },
   );
 };
 
 export const generateMeta = (
   title: string,
   description: string,
-  imageBaseUrl?: string
+  imageBaseUrl?: string,
 ): Metadata => ({
   title,
   description,
