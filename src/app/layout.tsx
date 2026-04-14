536769788d1384500021577629f40abe1d339138
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-04-14 20:06:53 +0300

    Add chat widget script to RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { SessionProvider } from "next-auth/react";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://biotech.cedne.ru"),
@@ -36,6 +37,19 @@ export default function RootLayout({
         </SessionProvider>
         {children}
         <Footer />
+        <Script
+          data-embed-id="e4675477-ad57-4532-a1fc-1a3a966adf8d"
+          data-base-api-url="https://llm.cedne.netcraze.link/api/embed"
+          src="https://llm.cedne.netcraze.link/embed/anythingllm-chat-widget.min.js"
+          data-language="ru"
+          data-chat-icon="chatBubble"
+          data-brand-image-url="/favicon.ico"
+          data-assistant-icon="/favicon.ico"
+          data-no-sponsor="true"
+          data-assistant-name="Секретарь факультета биотехнологий"
+          data-support-email="biotech@itmo.ru"
+          data-greeting="Привет! Я Секретарь факультета биотехнологий. Чем могу помочь?"
+        />
       </body>
     </html>
   );
