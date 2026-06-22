582f6cebf90a4c0c3bce3b970be891f8435f4a7b
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-06-23 02:47:01 +0300

    refactor(admin): stack list add-forms and align item actions on mobile

--- src/app/admin/scientific-works/page.client.tsx
+++ src/app/admin/scientific-works/page.client.tsx
@@ -30,32 +30,34 @@ export default function ScientificWorksClient({
     <Space orientation="vertical" className="w-full">
       <Form
         form={form}
-        layout="inline"
         className="w-full"
         onFinish={(values) =>
           runAction(addScientificWork(values.title), () => form.resetFields())
         }
       >
-        <Form.Item
-          name="title"
-          label="Название"
-          className="!flex-1"
-          rules={[
-            { required: true, message: "Укажите название" },
-            {
-              validator: (_, value) => {
-                if (scientificWorks.some((d) => d.title === value))
-                  return Promise.reject(new Error("Название должно быть уникальным"));
-                return Promise.resolve();
+        <div className="flex flex-col gap-2 sm:flex-row">
+          <Form.Item
+            name="title"
+            className="!mb-0 flex-1"
+            rules={[
+              { required: true, message: "Укажите название" },
+              {
+                validator: (_, value) => {
+                  if (scientificWorks.some((d) => d.title === value))
+                    return Promise.reject(
+                      new Error("Название должно быть уникальным"),
+                    );
+                  return Promise.resolve();
+                },
               },
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Button htmlType="submit" type="primary">
-          Добавить
-        </Button>
+            ]}
+          >
+            <Input placeholder="Название" />
+          </Form.Item>
+          <Button htmlType="submit" type="primary">
+            Добавить
+          </Button>
+        </div>
       </Form>
       <List
         dataSource={scientificWorks.filter((d) =>
@@ -67,48 +69,52 @@ export default function ScientificWorksClient({
           const isEditingNotCurrent = currentItem && currentItem.id !== item.id;
 
           return (
-            <List.Item
-              key={item.id}
-              actions={[
-                <Button
-                  key={`edit-btn-${item.id}`}
-                  disabled={isEditingNotCurrent}
-                  onClick={() => {
-                    if (!isEditing) setCurrentItem(item);
-                    else {
-                      runAction(updateScientificWork(currentItem!), () =>
-                        setCurrentItem(undefined)
-                      );
-                    }
-                  }}
-                  icon={isEditing ? <SaveOutlined /> : <EditOutlined />}
-                />,
-                <Popconfirm
-                  key={`delete-btn-${item.id}`}
-                  title="Удалить?"
-                  onConfirm={() => {
-                    if (isEditing) return setCurrentItem(undefined);
-                    else return runAction(deleteScientificWork(item.id));
-                  }}
-                >
+            <List.Item key={item.id}>
+              <div className="flex w-full items-start gap-3">
+                <div className="min-w-0 flex-1">
+                  {isEditing ? (
+                    <Input
+                      value={currentItem?.title}
+                      onChange={(e) =>
+                        setCurrentItem((i) => ({ ...i!, title: e.target.value }))
+                      }
+                    />
+                  ) : (
+                    item.title
+                  )}
+                </div>
+                <div className="flex shrink-0 gap-1">
                   <Button
-                    danger
+                    size="small"
+                    type="text"
                     disabled={isEditingNotCurrent}
-                    icon={isEditing ? <CloseOutlined /> : <DeleteOutlined />}
+                    onClick={() => {
+                      if (!isEditing) setCurrentItem(item);
+                      else {
+                        runAction(updateScientificWork(currentItem!), () =>
+                          setCurrentItem(undefined)
+                        );
+                      }
+                    }}
+                    icon={isEditing ? <SaveOutlined /> : <EditOutlined />}
                   />
-                </Popconfirm>,
-              ]}
-            >
-              {isEditing ? (
-                <Input
-                  value={currentItem?.title}
-                  onChange={(e) =>
-                    setCurrentItem((i) => ({ ...i!, title: e.target.value }))
-                  }
-                />
-              ) : (
-                item.title
-              )}
+                  <Popconfirm
+                    title="Удалить?"
+                    onConfirm={() => {
+                      if (isEditing) return setCurrentItem(undefined);
+                      else return runAction(deleteScientificWork(item.id));
+                    }}
+                  >
+                    <Button
+                      size="small"
+                      type="text"
+                      danger
+                      disabled={isEditingNotCurrent}
+                      icon={isEditing ? <CloseOutlined /> : <DeleteOutlined />}
+                    />
+                  </Popconfirm>
+                </div>
+              </div>
             </List.Item>
           );
         }}
